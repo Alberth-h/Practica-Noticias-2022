398f7cd0e2edaaf52efaba13e7eeec42ddec0a00
Alumno <ulsa@D109.local>
2022-01-31 18:48:22 -0700

    BD y env

--- Noticiero.sql
+++ Noticiero.sql
@@ -1,24 +1,17 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 31, 2022 at 10:39 PM
--- Server version: 5.7.34
--- PHP Version: 7.4.21
+-- Generation Time: Feb 01, 2022 at 01:44 AM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `Noticiero`
+-- Database: `noticiero`
 --
 
 -- --------------------------------------------------------
@@ -42,7 +35,8 @@ CREATE TABLE `noticias` (
 --
 
 INSERT INTO `noticias` (`id`, `titulo`, `autor`, `fecha`, `noticia`, `created_at`, `updated_at`) VALUES
-(1, 'Gato se cayó', 'Carlos Encinas', '2022-01-24', 'noooooooo se cayó un gatooooo!!!\r\n\r\nMIUAAAAUUUU', '2022-01-13 01:41:31', '2022-01-14 01:41:31');
+(1, 'Gato se cayó', 'Carlos Encinas', '2022-01-24', 'noooooooo se cayó un gatooooo!!!\r\n\r\nMIUAAAAUUUU\r\n\r\nNOOOOOO', '2022-02-01 01:16:30', '2022-02-01 08:16:30'),
+(2, 'Guerra entre rusia y ucrania siempre si afectara el precio de la tortilla en mexico', 'KiuboNews', '2022-02-01', 'El conflicto belico entre rusia y ucrania parece que si impactara el precio de la tortilla, habitantes mexicanos estan preocupados por la inflacion del alimento, se cree que valdra $40 el kilo', '2022-02-01 01:03:09', '2022-02-01 08:03:09');
 
 --
 -- Indexes for dumped tables
@@ -62,9 +56,4 @@ ALTER TABLE `noticias`
 -- AUTO_INCREMENT for table `noticias`
 --
 ALTER TABLE `noticias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
